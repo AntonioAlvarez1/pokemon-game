673cc54bec4264f2a4c56b48d9af737ce4faefff
Antonio Alvarez <antonio.alvarez98a@gmail.com>
2023-08-17 12:59:42 -0600

    First Commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+   import PokemonOption from '@/components/PokemonOptions.vue';
+   import PokemonPicture from '@/components/PokemonPicture.vue';
+   export default{
+    components:{PokemonOption,PokemonPicture}
+   }
+
+</script>
+
+<template>
+    <h1> quien es este Pokemon </h1>
+    <PokemonPicture :pokemonId="4" :showPokemon="false"/>
+    <PokemonOption/>
+
+</template>
+
+<style>
+
+</style>
